8316c64e33674dafdd39fb474f1693e2579cdf3c
Mariano Ravinale <mravinale@gmail.com>
2015-10-11 20:35:54 -0300

    removed .cgz dependency from home js

--- www/js/home.js
+++ www/js/home.js
@@ -120,7 +120,7 @@
 	document.title = "Cargando...";
 
 	// Insert main application stylesheet
-	insertStyleSheet( assetLocation + "css/app." + ( ver < 210 ? "css" : "cgz" ) );
+	insertStyleSheet( assetLocation + "css/app." +"css"); //( ver < 210 ? "css" : "cgz" ) );
 
 	// Insert favicon for web page
 	insertStyleSheet( assetLocation + "img/favicon.ico", "shortcut icon" );
