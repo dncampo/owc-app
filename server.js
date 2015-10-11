51188bc15f2022aff59c53c59a2c523a967f0cc6
Mariano Ravinale <mravinale@gmail.com>
2015-10-11 18:43:30 -0300

    override default mime type configuration

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.use(cors());
 app.use(express.static(__dirname + '/www'));
 app.use(express.static(__dirname + '/build'));
 
+express.static.mime.define({'text/css': ['cgz']});
+express.static.mime.define({'text/javascript': ['jgz']});
+
 
 var server = app.listen(process.env.PORT || 3000,  function(){
   console.log('Express: listening port: ', server.address().port);
